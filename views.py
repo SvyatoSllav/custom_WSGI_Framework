2f565c0798fc8cc50a878e49d68b59306ed9a131
Александр <tarasov.slavas2002@gmail.com>
2022-06-28 20:10:17 +0000

    added request dict to params of Index func

--- views.py
+++ views.py
@@ -2,5 +2,8 @@ from even_framework.templator import render
 
 
 class Index:
-    def __call__(self):
-        return '200 OK', render('index.html', title='title')
+    def __call__(self, request):
+        return '200 OK', render(
+            'index.html',
+            title='title',
+        )
