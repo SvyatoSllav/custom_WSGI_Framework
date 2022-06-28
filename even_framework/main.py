bbbc9923d97a45ec22fc9365e452db8ed14a9841
Александр <tarasov.slavas2002@gmail.com>
2022-06-28 20:13:07 +0000

    created condition check for POST and GET request. Created request dict for view

--- even_framework/main.py
+++ even_framework/main.py
@@ -1,5 +1,9 @@
+import quopri
+from .framework_requests import GetRequest, PostRequest
+
+
 class PageNotFound404:
-    def __call__(self):
+    def __call__(self, request):
         return '404 NOT FOUND', 'Cтраница не найдена'
 
 
@@ -13,12 +17,34 @@ class Framework:
         if not path.endswith('/'):
             path = f'{path}/'
 
+        request = {}
+        method = environ.get('REQUEST_METHOD', None)
+        request['method'] = method
+
+        if method == 'GET':
+            request_params = GetRequest().get_request_params(environ)
+            request['request_params'] = request_params
+            print(f'Нам пришли GET-параметры: {request_params}')
+
+        elif method == 'POST':
+            data = PostRequest().get_request_params(environ)
+            request['data'] = data
+            print(f'Пришел POST request: {Framework.decode_value(data)}')
+
         if path in self.routes_list:
             view = self.routes_list[path]
         else:
             view = PageNotFound404()
-        print(view)
-        code, body = view()
 
+        code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for key, value in data.items():
+            val = bytes(value.replace('%', '=').replace('+', ' '), 'UTF-8')
+            val_decode_str = quopri.decodestring(val).decode('UTF-8')
+            new_data[key] = val_decode_str
+        return new_data
