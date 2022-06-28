359d361197bda6c9235aeb08cb6f0cb10b0be5a2
Александр <tarasov.slavas2002@gmail.com>
2022-06-28 20:46:45 +0000

    added ability to inherit from other templates

--- even_framework/templator.py
+++ even_framework/templator.py
@@ -1,10 +1,9 @@
-from os.path import join
-
-from jinja2 import Template
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
-    file_path = join(folder, template_name)
-    with open(file_path, encoding='utf-8') as f:
-        template = Template(f.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
