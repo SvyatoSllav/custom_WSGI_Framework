a1770fcd2ed16bd4f28ce1026aa76e9edc197685
Александр <tarasov.slavas2002@gmail.com>
2022-07-07 16:20:07 +0000

    created models for student, teacher, interactive and record courses, categories and Engine for manipulating on classes

---
+++ components/models.py
@@ -0,0 +1,117 @@
+import quopri
+
+
+# Abstract user class
+class User:
+    pass
+
+
+# Student class
+class Student(User):
+    pass
+
+
+# Teacher class
+class Teacher(User):
+    pass
+
+
+class UserFactory:
+    """Creates Student or Teacher object"""
+
+    types = {
+        'student': Student,
+        'teacher': Teacher
+    }
+
+    @classmethod
+    def create_user(cls, type_):
+        return cls.types[type_]()
+
+
+class Category:
+
+    auto_id = 0
+
+    def __init__(self, name, category):
+        self.id = Category.auto_id
+        Category.auto_id += 1
+        self.name = name
+        self.category = category
+        self.courses = []
+
+    def course_count(self):
+        result = len(self.courses)
+        if self.category:
+            result += self.category.course_count()
+        return result
+
+    def create(cls, type_, name, category):
+        return cls.types[type_](name, category)
+
+
+# Abstarct class course
+class Course:
+    def __init__(self, name, category) -> None:
+        self.name = name
+        self.category = category
+        self.category.courses.append(self)
+
+
+class InteractiveCourse(Course):
+    pass
+
+
+class RecordCourse(Course):
+    pass
+
+
+class CourseFactory:
+    """Creates Interactive or Record course object."""
+    types = {
+        'interactive': InteractiveCourse,
+        'record': RecordCourse
+    }
+
+    @classmethod
+    def create(cls, type_, name, category):
+        return cls.types[type_](name, category)
+
+
+class Engine:
+    def __init__(self) -> None:
+        self.teacher = []
+        self.students = []
+        self.courses = []
+        self.categories = []
+
+    @staticmethod
+    def create_user(type_):
+        return UserFactory.create(type_)
+
+    @staticmethod
+    def create_category(name, category=None):
+        return Category(name, category)
+
+    def find_category_by_id(self, id):
+        for item in self.categories:
+            print('item', item.id)
+            if item.id == id:
+                return item
+        raise Exception(f'Нет категории с id = {id}')
+
+    @staticmethod
+    def create_course(type_, name, category):
+        return CourseFactory.create(type_, name, category)
+
+    def get_course(self, name):
+        for item in self.courses:
+            if item.name == name:
+                return item
+        return None
+
+    @staticmethod
+    def decode_value(val):
+        val_b = bytes(val.replace('%', '=').replace("+", " "), 'UTF-8')
+        val_decode_str = quopri.decodestring(val_b)
+        return val_decode_str.decode('UTF-8')
