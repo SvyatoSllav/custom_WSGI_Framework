8ded343fcfe4ea237d03d0d303b5c2cf7770b69c
Александр <tarasov.slavas2002@gmail.com>
2022-07-07 16:04:06 +0000

    added urls to new views

--- urls.py
+++ urls.py
@@ -1,5 +1,13 @@
-from views import Index
+from views import Index, About, StudyPrograms, CoursesList, \
+    CreateCourse, CreateCategory, CategoryList
+
 
 urls = {
-    '/': Index()
+    '/': Index(),
+    '/about/': About(),
+    '/study_programs/': StudyPrograms(),
+    '/courses-list/': CoursesList(),
+    '/create-course/': CreateCourse(),
+    '/create-category/': CreateCategory(),
+    '/category-list/': CategoryList()
 }
