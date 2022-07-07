5ef76dac3ee931e2f14b33cf99bb4a0679b0e738
Александр <tarasov.slavas2002@gmail.com>
2022-07-07 16:18:48 +0000

    added views that are realises create and read functionality for courses and categories

--- views.py
+++ views.py
@@ -1,9 +1,120 @@
+from datetime import date
+
 from even_framework.templator import render
 
+from components.models import Engine
 
+site = Engine()
+
+
+# Класс-контроллер - Главная страница
 class Index:
     def __call__(self, request):
-        return '200 OK', render(
-            'index.html',
-            title='title',
-        )
+        return '200 OK', render('index.html', objects_list=site.categories)
+
+
+# Класс-контроллер - Страница "О проекте"
+class About:
+    def __call__(self, request):
+        return '200 OK', render('about.html')
+
+
+# Класс-контроллер - Страница "Расписания"
+class StudyPrograms:
+    def __call__(self, request):
+        return '200 OK', render('study-programs.html', data=date.today())
+
+
+# Класс-контроллер - Страница 404
+class NotFound404:
+    def __call__(self, request):
+        return '404 WHAT', '404 PAGE Not Found'
+
+
+# Класс-контроллер - Страница "Список курсов"
+class CoursesList:
+    def __call__(self, request):
+
+        try:
+            category = site.find_category_by_id(
+                int(request['request_params']['id']))
+            return '200 OK', render('course_list.html',
+                                    objects_list=category.courses,
+                                    name=category.name,
+                                    id=category.id)
+        except KeyError:
+            return '200 OK', 'No courses have been added yet'
+
+
+# Класс-контроллер - Страница "Создать курс"
+class CreateCourse:
+    category_id = -1
+
+    def __call__(self, request):
+        if request['method'] == 'POST':
+
+            data = request['data']
+
+            name = data['name']
+            name = site.decode_value(name)
+
+            category = None
+            if self.category_id != -1:
+                category = site.find_category_by_id(int(self.category_id))
+
+                course = site.create_course('record', name, category)
+                site.courses.append(course)
+
+            return '200 OK', render('course_list.html',
+                                    objects_list=category.courses,
+                                    name=category.name,
+                                    id=category.id)
+
+        else:
+            try:
+                self.category_id = int(request['request_params']['id'])
+                print(f'ахх {request}')
+                category = site.find_category_by_id(int(self.category_id))
+
+                return '200 OK', render('create_course.html',
+                                        name=category.name,
+                                        id=category.id)
+            except KeyError:
+                return '200 OK', 'No categories have been added yet'
+
+
+# Класс-контроллер - Страница "Создать категорию"
+class CreateCategory:
+    def __call__(self, request):
+
+        if request['method'] == 'POST':
+
+            print(request)
+            data = request['data']
+
+            name = data['name']
+            name = site.decode_value(name)
+
+            category_id = data.get('category_id')
+
+            category = None
+            if category_id:
+                category = site.find_category_by_id(int(category_id))
+
+            new_category = site.create_category(name, category)
+
+            site.categories.append(new_category)
+
+            return '200 OK', render('index.html',
+                                    objects_list=site.categories)
+        else:
+            categories = site.categories
+            return '200 OK', render('create_category.html',
+                                    categories=categories)
+
+
+# Класс-контроллер - Страница "Список категорий"
+class CategoryList:
+    def __call__(self, request):
+        return '200 OK', render('category_list.html',
+                                objects_list=site.categories)
